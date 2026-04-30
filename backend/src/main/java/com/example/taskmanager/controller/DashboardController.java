bfeca0a8e17a1a7004cb254411eb52a35b4f76b5
HarshitSinha090104 <harshitsinha@gmail.com>
2026-04-30 20:40:09 +0530

    Modifications

--- backend/src/main/java/com/example/taskmanager/controller/DashboardController.java
+++ backend/src/main/java/com/example/taskmanager/controller/DashboardController.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 import java.util.*;
 
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/dashboard")
 @RequiredArgsConstructor
